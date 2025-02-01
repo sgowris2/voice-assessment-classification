b8e8c462b85592e79178a8476fbd6aa04993fd55
sgowris2 <sgowris2@gmail.com>
2025-02-01 23:08:09 +0530

    Defined levels in prompt and using smaller model

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import yaml
 
 
 def initialize_model(name='gemini-1.5-pro',
-                     temperature=1,
+                     temperature=1.0,
                      top_p=0.95,
                      top_k=40,
                      max_output_tokens=8192,
@@ -44,49 +44,53 @@ def upload_files(directory='../data'):
 
 def create_prompt():
 
-    prompt = [
+    prompt = []
+
+    prompt.extend([
         '''
-        Here is a CSV that contains information about Hindi reading assessments of students.
-        The first column of the CSV contains the filename of the voice recording for that student's assessment.
-        The second column of the CSV contains the level of the content that they are reading in that recording. 
-        The third column of the CSV contains the reading level that has been assigned to that student's recording. 
-        Note that the reading level can never be higher than the level of the content in a recording. 
-        The files with the same display_name have been uploaded into the Gemini system. 
-        Learning from this data, help me assess the reading level of the recordings with the following display_names:  
-        ['0241', '0246', '0253', '0254', '0271'].
-        The content level of these recordings is 6. 
-        Give an answer in JSON format that looks like 
-        {output: [{"filename": [string], "content_level": [int], "reading_level": [int], "reason_for_reading_level": [string]}, {...}, ...]
-        ''',
+        Here is a recording of a student reading a Hindi text (short story).
+        The actual text that the student is reading is the following:
+        ```
+        नद; पल; फल; बज; भर; ह; न; ड़; आ; छ;
+        ```
+        The level of this text is: Level 2
+        
+        For the recording file, do the following:
+        1. First, create a transcript from the recording and compare the recording transcript with the actual text.
+        2. Then, assess the reading level of the student using the reading levels which are given as follows:
+            ```
+            Level 1 - Unable to recognize most letters.
+            Level 2 - Pre-reader: Recognizes letters (like क, ख, ग, etc.). But unable to read any words.
+            Level 3 - Beginner Word Reader: Reads only very short, common words with mistakes and multiple attempts.
+            Level 4 - Word Reader: Reads simple words with limited mistakes. Can read simple sentences in a word-by-word fashion.
+            Level 5 - Sentence Reader: Reads sentences but with limited comprehension and inaccurate intonation.
+            Level 6 - Paragraph Reader: Reads sentences fluently with reasonable comprehension and mostly correct intonation.
+            Level 7 - Story Reader: Reads a more complex Hindi text with fluency, comprehension, and no mistakes.
+            ```
+        
+        Note that the reading level of a student cannot be assessed higher than the level of the text.
+        
+        *
+        Give an answer in JSON format that looks like:
+        {output: [{"reading_level": int, "transcript": string, "reason_for_reading_level": string}, {...}, ...]
         '''
-        CSV with Hindi reading assessments of students: \n
-        filename,content_level,reading_level
-        0003,6,6
-        0004,6,6
-        0011,6,5
-        0012,6,5
-        0020,5,4
-        0024,5,4
-        0072,4,3
-        0085,4,3
-        0098,5,5
-        0177,4,4
-        0194,2,2
-        0297,6,2
-        ''',
         ]
+    )
 
     audio_files = genai.list_files()
-
     for f in audio_files:
-        prompt.append(genai.get_file(f.name))
+        if f.display_name in ['0446']:
+            prompt.append(genai.get_file(f.name))
 
     return prompt
 
 if __name__ == '__main__':
-    model = initialize_model()
+    model = initialize_model(name='gemini-1.5-flash-8b', temperature=0.4, top_k=3, top_p=0.75)
     upload_files()
     prompt = create_prompt()
     result = model.generate_content(prompt)
     print(result.text)
     pass
+
+
+
